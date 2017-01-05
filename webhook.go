203a4429b29d3baeedb3f30126b6b5c7e4fbc261
Georgy Buranov <gburanov@gmail.com>
2017-01-05 18:53:34 +0100

    continue working on web hooks

--- webhook.go
+++ webhook.go
@@ -1,8 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+
+	githubhook "gopkg.in/rjz/githubhook.v0"
+)
+
+const secret string = "12345"
 
 func webhookHandler(ctx *Context) error {
-	fmt.Printf("EVENT")
+	hook, err := githubhook.Parse([]byte(secret), ctx.Req)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	fmt.Println("Processing hook " + hook.Event)
+	if hook.Event == "pull_request" {
+		processPullRequest(hook.Payload)
+	}
+	return nil
+}
+
+type PullRequest struct {
+	Number int `json:"number"`
+}
+
+type PullRequestChange struct {
+	Action string      `json:"action"`
+	Pr     PullRequest `json:"pull_request"`
+}
+
+func processPullRequest(payload []byte) error {
+	change := PullRequestChange{}
+	err := json.Unmarshal(payload, &change)
+	if err != nil {
+		return err
+	}
+	fmt.Println("What changed " + change.Action)
 	return nil
 }
